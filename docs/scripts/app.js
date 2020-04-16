61f77e3f424651c108dc7c8450163084f97ce473
Rodney Guillen <infrony@gmail.com>
2020-04-15 20:09:27 -0500

    fixing github pages

--- docs/scripts/app.js
+++ docs/scripts/app.js
@@ -149,7 +149,7 @@ function renderForecast(card, data) {
  * @return {Object} The weather forecast, if the request fails, return null.
  */
 function getForecastFromNetwork(coords) {
-  return fetch(`/forecast/${coords}`)
+  return fetch(`/testing-pwapp/forecast/${coords}`)
     .then(response => {
       return response.json();
     })
